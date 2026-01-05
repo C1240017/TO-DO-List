060b6a7b12c0e0d88af8c24401747be8854e7ea7
Mahad ahmed <ahmedmahad235@gmail.com>
2026-01-05 05:43:26 -0800

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,38 @@
+let inputbox=document.getElementById("input-box");
+let list_container=document.getElementById("list-container");
+
+function AddTask(){
+    if(inputbox.value=== ''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li")
+        li.textContent=inputbox.value;
+        list_container.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span)
+    }
+    inputbox.value='';
+    saveData()
+}
+
+list_container.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+function saveData(){
+    localStorage.setItem("data",list_container.innerHTML);
+}
+
+function showTask(){
+    list_container.innerHTML=localStorage.getItem("data")
+}
+showTask()
